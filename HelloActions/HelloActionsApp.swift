85a2f2685601338ba6c77fe5c0850e1d04173260
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2022-06-09 23:18:11 +0300

    initial commit

---
+++ HelloActions/HelloActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloActionsApp.swift
+//  HelloActions
+//
+//  Created by Mohammad Rahchamani on 6/9/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
